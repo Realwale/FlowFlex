a956eb22d9b065594949ea19ab13ca5e5f3ed133
Realwale <olawaleagboola41@gmail.com>
2024-08-19 11:27:33 +0100

    initialise project

---
+++ src/main/java/com/charistech/flowflex/backend/FlowFlexApplication.java
@@ -0,0 +1,13 @@
+package com.charistech.flowflex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowFlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charistech/flowflex/backend/FlowFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charistech.flowflex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowFlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
